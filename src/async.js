d811f54956c178d6fe661211784ad14b02bf2d57
Jeremy McLeod <isochronous@gmail.com>
2013-01-10 15:52:11 -0500

    rebuilt without templatecache

--- src/async.js
+++ src/async.js
@@ -8,7 +8,6 @@ Backbone.Marionette.Async = (function(Backbone, Marionette, _, $){
   // Configure Marionette to use the async rendering for all view types
   var Async = {
     init: function(){
-      Marionette.TemplateCache = Async.TemplateCache;
       Marionette.Renderer = Async.Renderer;
       _.extend(Marionette.ItemView.prototype, Async.ItemView);
       _.extend(Marionette.CollectionView.prototype, Async.CollectionView);
@@ -22,9 +21,8 @@ Backbone.Marionette.Async = (function(Backbone, Marionette, _, $){
 //= backbone.marionette.async.compositeview.js
 //= backbone.marionette.async.region.js
 //= backbone.marionette.async.renderer.js
-//= backbone.marionette.async.templatecache.js
 //= backbone.marionette.async.helpers.js
 //= async.init.js
-  
+
   return Async;
 })(Backbone, Backbone.Marionette, _, window.jQuery || window.Zepto || window.ender);
